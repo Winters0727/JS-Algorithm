55d49e8f7bf4e55dec71b8059fbdf862f23714e2
winters0727 <winters0727@gmail.com>
2022-03-02 14:46:30 +0900

    refactor: LIS

--- src/Sets/LIS/index.js
+++ src/Sets/LIS/index.js
@@ -1,4 +1,13 @@
 const LIS = (set) => {
+  const getIndexArray = (array, maxLength) => {
+    return array
+      .map((length, index) => {
+        return { length, index };
+      })
+      .filter(({ length, index }) => length === maxLength)
+      .map(({ length, index }) => index);
+  };
+
   const length = set.length;
   const lengthArray = Array(length).fill(1);
 
@@ -10,7 +19,34 @@ const LIS = (set) => {
     }
   }
 
-  return Math.max(...lengthArray);
+  const maxLength = Math.max(...lengthArray);
+  const maxIndexes = getIndexArray(lengthArray, maxLength);
+  const answer = [];
+
+  while (maxIndexes.length > 0) {
+    const maxIndex = maxIndexes.pop();
+    const stack = [[maxLength, maxIndex, [set[maxIndex]]]];
+
+    while (stack.length > 0) {
+      const [length, currentIndex, sequence] = stack.pop();
+
+      if (length === 1) {
+        answer.push(sequence);
+        continue;
+      }
+
+      const indexArray = getIndexArray(lengthArray, length - 1);
+      indexArray.forEach((index) => {
+        if (index < currentIndex && set[index] < sequence[0]) {
+          const copiedSequence = sequence.slice();
+          copiedSequence.unshift(set[index]);
+          stack.push([length - 1, index, copiedSequence]);
+        }
+      });
+    }
+  }
+
+  return answer;
 };
 
 module.exports = LIS;
